93180ec702980cfb8271ceb54217ba8d7a81e83a
Ohiole <eboreimeojo99@gmail.com>
2025-01-20 20:12:47 +0100

    Reply to a reply

--- app/components/NewComment.tsx
+++ app/components/NewComment.tsx
@@ -2,36 +2,37 @@ import React from 'react'
 import Image from "next/image"
 
 interface NewCommentProps {
-    text: string; // You can add more props if needed
-    commentId?: number;
-    isReplyId?: number | null;
+  text: string; // You can add more props if needed
+  commentId?: number;
+  isReplyId?: number | null;
+  replyToReply?: boolean;
 }
 
-const NewComment: React.FC<NewCommentProps> = ({ text, commentId, isReplyId }) => {
+const NewComment: React.FC<NewCommentProps> = ({ text, commentId, isReplyId, replyToReply }) => {
   return (
     text === "reply" ?
-    <section className={`bg-white p-4 rounded-md ${commentId === isReplyId ? 'scale-100 opacity-100' : 'scale-0 opacity-0 absolute'} flex justify-between items-start gap-4 w-[600px] mb-2 transition duration-200`}>
+      <section className={`bg-white p-4 rounded-md ${commentId === isReplyId ? 'scale-100 opacity-100' : 'scale-0 opacity-0 absolute'} flex justify-between items-start gap-4 ${ replyToReply ? 'w-[550px]' : 'w-[600px]' } mb-2 transition duration-200`}>
         <Image src="/images/avatars/image-juliusomo.png" alt='User avatar' width={30} height={30} className="rounded-full" />
 
-        <textarea 
-            name="new-comment" 
-            placeholder='Add a comment...' 
-            className='w-3/4 rounded-md px-4 py-2 border-gray-200 border-solid border-[1px] outline-purple-300 transition duration-200 h-[100px] text-sm  text-gray-400'>
+        <textarea
+          name="new-comment"
+          placeholder='Add a comment...'
+          className='w-3/4 rounded-md px-4 py-2 border-gray-200 border-solid border-[1px] outline-purple-300 transition duration-200 h-[100px] text-sm  text-gray-400'>
         </textarea>
 
         <button className='py-2 px-4 bg-purple cursor-pointer text-sm text-background rounded-md hover:bg-purple-300 transition duration-200'>REPLY</button>
-    </section> :
-    <section className={`bg-white p-4 rounded-md flex justify-between items-start gap-4 w-[600px] mb-2 transition duration-200`}>
-    <Image src="/images/avatars/image-juliusomo.png" alt='User avatar' width={30} height={30} className="rounded-full" />
+      </section> :
+      <section className={`bg-white p-4 rounded-md flex justify-between items-start gap-4 w-[600px] mb-2 transition duration-200`}>
+        <Image src="/images/avatars/image-juliusomo.png" alt='User avatar' width={30} height={30} className="rounded-full" />
 
-    <textarea 
-        name="new-comment" 
-        placeholder='Add a comment...' 
-        className='w-3/4 rounded-md px-4 py-2 border-gray-200 border-solid border-[1px] outline-purple-300 transition duration-200 h-[100px] text-sm  text-gray-400'>
-    </textarea>
+        <textarea
+          name="new-comment"
+          placeholder='Add a comment...'
+          className='w-3/4 rounded-md px-4 py-2 border-gray-200 border-solid border-[1px] outline-purple-300 transition duration-200 h-[100px] text-sm  text-gray-400'>
+        </textarea>
 
-    <button className='py-2 px-4 bg-purple cursor-pointer text-sm text-background rounded-md hover:bg-purple-300 transition duration-200'>SEND</button>
-</section>
+        <button className='py-2 px-4 bg-purple cursor-pointer text-sm text-background rounded-md hover:bg-purple-300 transition duration-200'>SEND</button>
+      </section>
   )
 }
 
